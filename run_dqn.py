d0613510fb2cb76cc0fdfd6ed786d77480ce2bfe
maxpridy <maxpridy@gmail.com>
2021-08-04 21:15:40 +0900

    fix run_dqn.py

--- run_dqn.py
+++ run_dqn.py
@@ -26,10 +26,6 @@ class RLMgr:
         self.update_target_model()
         self.t_model.eval()
         
-        if True:
-            pass
-        else:
-            raise Exception('wrong model')
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
 
     def update_target_model(self):
